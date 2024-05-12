ae4bba769ea998fa51838cae7749d878c31122bf
kastrahl <kushagraarora123@gmail.com>
2024-05-12 08:50:55 +0530

    updating simple home and task views
    how to make detail view ;
    custom name for html files;
    add different views;

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 #from . import views                 # For Functions, thus but now it's class so 
-from .views import TaskList
+from .views import TaskList, TaskDetail
 
 """urlpatterns = [
     path('', views.taskList, name='tasks'),            #base url thus empty string, view name
 ]"""
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='tasks'),            #
+    path('', TaskList.as_view(), name='tasks'),         # root URL - 
+    path('task/<int:pk>', TaskDetail.as_view(), name='task'),   # when clicked on list item - sub url <int:pk> - pk - primary key  
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 from .models import Task
 
 # Create your views here.
@@ -10,5 +12,13 @@ from .models import Task
     return HttpResponse('To Do List')"""
 
 #class based views 
-class TaskList(ListView):                   #Listeview return a template with query set of data
+class TaskList(ListView):                   # Listeview return a template with query set of data
     model = Task
+    context_object_name = 'tasks'           # to assign variable context in HTML / simple view
+
+    # detail view - when people click on an html item in for we display that particular view 
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'    # so we don't have to name is task_details.html default in django
