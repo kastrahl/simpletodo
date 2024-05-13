4d2258248cf9ff1ba3190a15ffb1c32fbe4e8ee9
kastrahl <kushagraarora123@gmail.com>
2024-05-13 07:18:45 +0530

    added base functionalities and navigation
    > delete task
    > new task form
    > update home for delete option
    > connected with views and urls

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #from . import views                 # For Functions, thus but now it's class so 
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete         # CLASSES FROM VIEWS FILES 
 
 """urlpatterns = [
     path('', views.taskList, name='tasks'),            #base url thus empty string, view name
@@ -9,4 +9,8 @@ from .views import TaskList, TaskDetail
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),         # root URL - 
     path('task/<int:pk>', TaskDetail.as_view(), name='task'),   # when clicked on list item - sub url <int:pk> - pk - primary key  
+    path('create-task/', TaskCreate.as_view(), name='task-create'),         # url accessed to create the task
+    path('update-task/<int:pk>', TaskUpdate.as_view(), name='task-update'),         # url accessed to update the task
+    path('delete-task/<int:pk>', TaskDelete.as_view(), name='task-delete'),         # url accessed to delete the task
+
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
+from django.views.generic.detail import DetailView  #description pane
+from django.views.generic.edit  import CreateView, UpdateView, DeleteView   # create form; update  prefill and modify the data
+from django.urls import reverse_lazy                # redirects user to certain part or page
 
 from .models import Task
 
@@ -16,9 +18,34 @@ class TaskList(ListView):                   # Listeview return a template with q
     model = Task
     context_object_name = 'tasks'           # to assign variable context in HTML / simple view
 
-    # detail view - when people click on an html item in for we display that particular view 
 
+
+# detail view - when people click on an html item in for we display that particular view 
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'    # so we don't have to name is task_details.html default in django
+
+class TaskCreate(CreateView):
+    model = Task
+    # by default createview gives model form to work with 
+    # representation of form based on model -> takes model and create all fields by default
+#   fields = ['title','description' etc]        instead use __all__
+    fields ='__all__'
+    #redirect user after submission to different page -> add it to createview
+    success_url = reverse_lazy('tasks')
+
+# create view
+# set model
+# set fields
+# set redirect value 
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
